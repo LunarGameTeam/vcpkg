32eec45105bedca5c66deac4e71eaa0586a6ac3c
pancystar <18089137929@163.com>
2023-10-24 00:20:34 +0800

    升级dma库

--- ports/d3d12-memory-allocator/portfile.cmake
+++ ports/d3d12-memory-allocator/portfile.cmake
@@ -2,16 +2,17 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO GPUOpen-LibrariesAndSDKs/D3D12MemoryAllocator 
-    REF 10f148cef0dfd34ae1a9373b9396beb1581c992a
-    SHA512 c99934a606ce5a5c9c59e05faf2e659bfad2e485b58aaf00f38219a6c89f17b62033f4a69935915f0d5269a4f0ecba41037b044913ae6f4077fa981eaab470c8
+    REPO LunarGameTeam/D3D12MemoryAllocator 
+    REF a936da058cc658da13dbd759732b1622488adf78
+    SHA512 8c9164d5d58263cf26936db48468bdb9b24c0967e747007b98eaa1bb74edccaf40a57b51c13cbeb1e028178b137cc602395870c0e107d9324c852e86ce646c02
     HEAD_REF master
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+     WINDOWS_USE_MSBUILD
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
