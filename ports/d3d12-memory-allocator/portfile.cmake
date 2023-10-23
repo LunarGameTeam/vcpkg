30d102161f6964e265281f0d4cfdc9c08581e3e6
pancystar <18089137929@163.com>
2023-10-24 01:55:31 +0800

    升级修复dma库的install信息不全的问题

--- ports/d3d12-memory-allocator/portfile.cmake
+++ ports/d3d12-memory-allocator/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO LunarGameTeam/D3D12MemoryAllocator 
-    REF a936da058cc658da13dbd759732b1622488adf78
-    SHA512 8c9164d5d58263cf26936db48468bdb9b24c0967e747007b98eaa1bb74edccaf40a57b51c13cbeb1e028178b137cc602395870c0e107d9324c852e86ce646c02
+    REF 50652df0dd5e2bec0442ed9bd059804328c36fa8
+    SHA512 dc49dc091dce9e59f817e0369b1fb24ff2a1aea852a51887d9b8ff467ddd3e0f9076a1a04d5f909a59cd45ba9d526a354f4ead43f51f195f7a6d4494eeb844da
     HEAD_REF master
 )
 
@@ -16,8 +16,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/d3d12-memory-allocator)
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(CONFIG_PATH cmake/)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
